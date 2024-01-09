af56d43865a316680fe45e59c552c7d5b6f225ee
mehedi-hasan <mehediinform@gmail.com>
2024-01-10 02:52:22 +0600

    2nd Commit android_Font

--- app/src/main/java/com/example/android_font/MainActivity.java
+++ app/src/main/java/com/example/android_font/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.android_font;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView1,textView2;
+    private Typeface typeface1,typeface2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView1 = findViewById(R.id.textView_Id1);
+        textView2 = findViewById(R.id.textView_Id2);
+
+        typeface1 = Typeface.createFromAsset(getAssets(),"font/OpenSans_Bold.ttf");
+        textView1.setTypeface(typeface1);
+
+
+        typeface2 = Typeface.createFromAsset(getAssets(),"font/OpenSans_Italic.ttf");
+        textView2.setTypeface(typeface2);
     }
 }
